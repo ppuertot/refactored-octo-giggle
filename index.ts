7051eb733926887a6a50bd62c08e65570284a18b
Pedro Puerto <pedro.puerto@gmail.com>
2023-11-14 12:01:00 -0600

    router

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
+import express, { Express, Request, Response, Router } from "express";
+import dotenv from "dotenv";
 dotenv.config();
+import { router } from "./src/router";
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server.');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server.");
 });
+
+app.use("/router", router);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ src/router.ts
@@ -0,0 +1,7 @@
+import express, { Express, Request, Response, Router } from "express";
+
+export const router: Router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server + Router");
+});
